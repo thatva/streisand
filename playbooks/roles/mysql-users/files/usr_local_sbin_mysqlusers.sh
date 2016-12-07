555cf7a1a0cbe704cc1b7c158c0eba89d62b7441
Demian Wandelow <demian@opentierra.com>
2016-12-06 23:08:52 -0300

    * Supress mysql output

--- playbooks/roles/mysql-users/files/usr_local_sbin_mysqlusers.sh
+++ playbooks/roles/mysql-users/files/usr_local_sbin_mysqlusers.sh
@@ -49,7 +49,7 @@ del_l2tpd_user() {
 }
 
 
-mapfile -t user < <(echo select name,secret,ssport,status from users | mysql -h $mysql_host -u $mysql_user -s -p${mysql_pass} $mysql_db 2>/dev/null)
+mapfile -t user < <(echo select name,secret,ssport,status from users | mysql -h $mysql_host -u $mysql_user -p${mysql_pass} $mysql_db 2>/dev/null)
 
 for u in $(seq 1 $((${#user[@]} - 1))); do
    user_attrs=( $(echo ${user[$u]}) )
